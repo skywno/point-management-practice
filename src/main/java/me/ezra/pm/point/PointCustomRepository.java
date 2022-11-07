ab85afa46301161f4bf5488dd52776f573f84849
Ezra Lim <skywno@gmail.com>
2022-11-07 11:41:49 +0200

    Implment a custom point repository using query dsl

---
+++ src/main/java/me/ezra/pm/point/PointCustomRepository.java
@@ -0,0 +1,13 @@
+package me.ezra.pm.point;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+public interface PointCustomRepository {
+
+    Page<ExpiredPointSummary> sumByExpiredDate(LocalDate alarmCriteriaDate, Pageable pageable);
+
+//    Page<ExpiredPointSummary> sumBeforeExpireDate(LocalDate alarmCriteriaDate, Pageable pageable);
+}
---
+++ src/main/java/me/ezra/pm/point/PointCustomRepositoryImpl.java
@@ -0,0 +1,54 @@
+package me.ezra.pm.point;
+
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.List;
+
+public class PointCustomRepositoryImpl extends QuerydslRepositorySupport implements PointCustomRepository {
+
+    /**
+     * Creates a new {@link QuerydslRepositorySupport} instance for the given domain
+     * type.
+     */
+    public PointCustomRepositoryImpl() {
+        super(Point.class);
+    }
+
+    @Override
+    public Page<ExpiredPointSummary> sumByExpiredDate(
+            LocalDate alarmCriteriaDate,
+            Pageable pageable) {
+        QPoint point = QPoint.point;
+        JPQLQuery<ExpiredPointSummary> query = from(point).select(
+                new QExpiredPointSummary(
+                        point.pointWallet.userId,
+                        point.amount.sum().coalesce(BigInteger.ZERO)
+                )
+                ).where(point.expired.eq(true))
+                .where(point.used.eq(false))
+                .where(point.expireDate.eq(alarmCriteriaDate))
+                .groupBy(point.pointWallet);
+        List<ExpiredPointSummary> expiredPointList = getQuerydsl().applyPagination(pageable, query).fetch();
+        long elementCount = query.fetchCount();
+        return new PageImpl<>(
+                expiredPointList,
+                PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()),
+                elementCount
+        );
+    }
+
+//    @Override
+//    public Page<ExpiredPointSummary> sumBeforeExpireDate(
+//            LocalDate alarmCriteriaDate,
+//            Pageable pageable
+//    ) {
+//        return null;
+//    }
+}
